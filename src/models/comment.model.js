1aa4fd496ae45c62518bf22ea55d0ce576ba1f37
Mohammad <mohammadweex21@gmail.com>
2024-06-19 09:31:51 +0500

    feat: Add comment, like, playlist, and tweet models

---
+++ src/models/comment.model.js
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from "mongoose"
+import mongooseAggregatePaginate from
+ "mongoose-aggregate-paginate-v2"
+
+const commentSchema=new Schema(
+    {
+        content:{
+            type:String,
+            required:true
+        },
+        video:{
+            type:Schema.Types.ObjectId,
+            ref:"Video",
+            required:true
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+          
+        },
+    }, // Add a closing parenthesis here
+    {timestamps:true}
+
+)
+
+commentSchema.plugin(mongooseAggregatePaginate)
+
+export const Comment=mongoose.model("Comment",commentSchema)
